11dd771fec00a63beba1fd746e71d698ef36c8bf
Franck <136079691+Freddyede@users.noreply.github.com>
2024-03-29 11:31:08 +0100

    Adding array started todos in separated file

---
+++ src/data/todos.jsx
@@ -0,0 +1,18 @@
+export const todos = [
+    {
+        label: "Lorem ipsum dolor sit amet,",
+        status: false
+    },
+    {
+        label: "consectetur adipisicing elit.",
+        status: false
+    },
+    {
+        label: "Aliquid asperiores cumque dicta fugit harum omnis placeat porro quis rerum.",
+        status: false
+    },
+    {
+        label: "Animi dolore quaerat quas quis quisquam, quo quos reprehenderit repudiandae! Minima?",
+        status: false
+    }
+];
